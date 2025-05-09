6645e08e484ba3ac109fa7bf1979053460c51973
Winstar <james.canoy87@gmail.com>
2025-05-09 21:41:05 +0800

    Adding function to get the list of quiz file in category

--- quiz_player.py
+++ quiz_player.py
@@ -3,3 +3,6 @@ import random
 
 def list_categories(path):
     return [category for category in os.listdir(path) if os.path.isdir(os.path.join(path, category))]
+
+def list_quiz_files(path):
+    return [file for file in os.listdir(path) if file.endswith(".txt")]
