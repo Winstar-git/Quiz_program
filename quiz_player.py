4b6871f62ee2ec50203197b7cbae3f52adf978b4
Winstar <james.canoy87@gmail.com>
2025-05-09 21:50:10 +0800

    Adding function quiz to read the quiz file

--- quiz_player.py
+++ quiz_player.py
@@ -6,3 +6,14 @@ def list_categories(path):
 
 def list_quiz_files(path):
     return [file for file in os.listdir(path) if file.endswith(".txt")]
+
+def quiz(filepath):
+    questions = []
+    try:
+        with open(filpath, r, encoding="utf-8") as file:
+            blocks = file.read().split("################")
+            for block in blocks:
+                line = block.strip()
+                if len(line) < 6:
+                    continue
+                
